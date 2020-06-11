a131987d3cdaa8b39b97bcb4c08f6d3a2e9e0343
Alex Marquardt <alluma92@gmail.com>
2020-06-11 13:06:27 +0200

    Small changes for production

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.use('/api/suggestions', suggestionRouter);
 
 /**** Building ****/
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname = 'client/build/index.html'))
+    res.sendFile(path.resolve('..', 'client', 'build', 'index.html'))
 );
 
 /**** Start ****/
