43af26035ad75f06e5f85176448175bdd4b3bcdd
Alex Marquardt <alluma92@gmail.com>
2020-06-11 12:06:50 +0200

    Adding suggestions for users signature

--- client/src/UserPage.jsx
+++ client/src/UserPage.jsx
@@ -2,6 +2,31 @@ import React, { Component } from "react";
 import { Link } from "@reach/router";
 
 class UserPage extends Component {
+  // finds username in suggestions object
+  findSuggestions(suggestions, usernameInput) {
+    const filteredSuggestions = [];
+
+    for (
+      let suggestionCount = 0;
+      suggestionCount < suggestions.length;
+      suggestionCount++
+    ) {
+      for (
+        let signatureCount = 0;
+        signatureCount < suggestions[suggestionCount].signatures.length;
+        signatureCount++
+      ) {
+        if (
+          suggestions[suggestionCount].signatures[signatureCount].username ===
+          usernameInput
+        ) {
+          filteredSuggestions.push(suggestions[suggestionCount]);
+        }
+      }
+    }
+    return filteredSuggestions;
+  }
+
   render() {
     const user = this.props.getUser(this.props.userId);
     const suggestions = this.props.suggestions;
@@ -25,9 +50,14 @@ class UserPage extends Component {
             <li className="list-group-item">
               <b>Suggestion with signatures:</b>
               <ul>
-                {suggestions.map((suggestion) => (
-                  <li>{suggestion.title}</li>
-                ))}
+                {this.findSuggestions(suggestions, user.username).map(
+                  (suggestion) => (
+                    <li key={suggestion._id}>
+                      Title:
+                      {suggestion.title}
+                    </li>
+                  )
+                )}
               </ul>
             </li>
           </ul>
